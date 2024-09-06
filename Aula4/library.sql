cfc5a4dc067b479180bcd0035b7beba74c4e3141
vJhonatan <jhonatan9963@gmail.com>
2024-09-05 21:35:11 -0300

    Aula 4 - Join com tabela categoria

--- Aula4/library.sql
+++ Aula4/library.sql
@@ -1,29 +1,40 @@
 CREATE DATABASE library;
 USE library;
 
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    categoria VARCHAR(100)
+);
+
 CREATE TABLE autor (
-	id_autor INT PRIMARY KEY AUTO_INCREMENT ,
+    id_autor INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100)
 );
 
 CREATE TABLE livro (
-	id_livro INT PRIMARY KEY AUTO_INCREMENT,
+    id_livro INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(100),
     id_autor INT,
-    FOREIGN KEY (id_autor) REFERENCES autor(id_autor)
+    id_categoria INT,
+    FOREIGN KEY (id_autor) REFERENCES autor(id_autor),
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
 
-INSERT INTO autor(nome) VALUE
+INSERT INTO autor(nome) VALUES
 ('Bruno Guimaraes'),
 ('Vinicius Jr');
 
-INSERT INTO livro(titulo,id_autor) VALUE
-('Harry Potter', 1),
-('Turma da Monica', 2),
-('Homem Aranha', 2);
+INSERT INTO categoria(categoria) VALUES
+('Ficção'),
+('Animação'),
+('Comedia');
 
-SELECT livro.titulo AS livro, autor.nome AS autor
+INSERT INTO livro(titulo, id_autor, id_categoria) VALUES
+('Harry Potter', 1, 1),
+('Turma da Monica', 2, 2),
+('Homem Aranha', 2, 1);
+
+SELECT livro.titulo AS livro, autor.nome AS autor, categoria.categoria AS categoria
 FROM livro
-JOIN autor ON livro.id_autor = autor.id_autor;
-
-
+JOIN autor ON livro.id_autor = autor.id_autor
+JOIN categoria ON livro.id_categoria = categoria.id_categoria;
